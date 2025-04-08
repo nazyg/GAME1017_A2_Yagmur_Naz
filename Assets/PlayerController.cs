43570632404ea8c66bf2ba0f9578e0baec531959
nazyg <114533761+nazyg@users.noreply.github.com>
2025-04-08 03:56:22 -0400

    cheated character and animations

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Animator animator; 
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(horizontal * moveSpeed, rb.linearVelocity.y);
+        animator.SetBool("isRunning", Mathf.Abs(horizontal) > 0);
+    }
+}
